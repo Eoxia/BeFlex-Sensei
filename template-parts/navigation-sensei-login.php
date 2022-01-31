b26532438cf6f43e8a91da7afb19e6ccc2a13eec
eoxia-amandine <amandine@eoxia.com>
2022-01-31 15:34:41 +0100

    Mise à jour bouton header + lien profile

--- template-parts/navigation-sensei-login.php
+++ template-parts/navigation-sensei-login.php
@@ -14,17 +14,18 @@ $my_account_page_id = intval( Sensei()->settings->settings['my_course_page'] );
 $my_courses_url     = get_permalink( $my_account_page_id );
 
 if ( is_user_logged_in() ) :
+	$profile_url = Sensei()->learner_profiles->get_permalink( get_current_user_id() );
 	?>
-	<?php if ( ! empty( $my_courses_url ) ) : ?>
-		<a href="<?php echo esc_url( $my_courses_url ); ?>"><i class="far fa-user"></i> <?php esc_html_e( 'My courses', 'beflex' ); ?> </a>
+	<?php if ( ! empty( $profile_url ) ) : ?>
+		<a href="<?php echo esc_url( $profile_url ); ?>" class="button button-primary"><i class="far fa-user"></i> <?php esc_html_e( 'My courses', 'beflex' ); ?> </a>
 	<?php endif; ?>
-	<a href="<?php echo esc_url( wp_logout_url( home_url() ) ); ?>"><?php esc_html_e( 'Logout', 'beflex' ); ?></a>
+	<a href="<?php echo esc_url( wp_logout_url( home_url() ) ); ?>"><i class="far fa-sign-out-alt"></i> <?php esc_html_e( 'Logout', 'beflex' ); ?></a>
 	<?php
 else :
 	if ( ! empty( $my_courses_url ) ) :
 		?>
-		<a href="<?php echo esc_url( $my_courses_url ); ?>"><i class="far fa-user"></i> <?php esc_html_e( 'Login', 'beflex' ); ?> </a>
-		<a href="<?php echo esc_url( $my_courses_url ); ?>"><?php esc_html_e( 'Sign in', 'beflex' ); ?></a>
+		<a href="<?php echo esc_url( $my_courses_url ); ?>" class="button button-primary"><i class="far fa-user"></i> <?php esc_html_e( 'Login', 'beflex' ); ?> </a>
+		<a href="<?php echo esc_url( $my_courses_url ); ?>"><i class="far fa-edit"></i> <?php esc_html_e( 'Sign in', 'beflex' ); ?></a>
 		<?php
 	endif;
 endif;
