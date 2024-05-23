84862870a374c6aacdabd957863c24ff5aade0ba
eoxia-amandine <amandine@eoxia.com>
2024-05-23 17:25:28 +0200

    #51 fix: Headers of old templates php

--- sensei/archive-course.php
+++ sensei/archive-course.php
@@ -8,23 +8,25 @@ Template Name: Example
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<?php
-	$block_content = do_blocks( file_get_contents(get_stylesheet_directory() . "/block-templates/archive-course.html") );
+	$block_header  = do_blocks( file_get_contents(get_stylesheet_directory_uri() . "/block-template-parts/header.html") );
+	$block_content = do_blocks( file_get_contents(get_stylesheet_directory_uri() . "/block-templates/archive-course.html") );
+	$block_footer  = do_blocks( file_get_contents(get_stylesheet_directory_uri() . "/block-template-parts/footer.html") );
 	?>
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class( 'sensei-template-old' ); ?>>
+<body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div class="wp-site-blocks">
 
 	<header class="wp-block-template-part site-header">
-		<?php block_header_area(); ?>
+		<?php echo $block_header; ?>
 	</header>
 
 	<?php echo $block_content; ?>
 
 	<footer class="wp-block-template-part site-footer">
-		<?php block_footer_area(); ?>
+		<?php echo $block_footer; ?>
 	</footer>
 </div>
 <?php wp_footer(); ?>
