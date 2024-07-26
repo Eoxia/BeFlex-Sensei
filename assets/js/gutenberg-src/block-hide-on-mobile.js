8b0474981b77969b1d112233367b869745bba591
eoxia-amandine <amandine@eoxia.com>
2024-07-26 11:22:47 +0200

    #57 [Attributes] Update namespace of custom attributes

--- assets/js/gutenberg-src/block-hide-on-mobile.js
+++ assets/js/gutenberg-src/block-hide-on-mobile.js
@@ -1,61 +1,107 @@
-function hideOnMobileAttribute(settings, name) {
-  if ( typeof settings.attributes !== 'undefined' ) {
-    settings.attributes = Object.assign(settings.attributes, {
-      hideOnMobile: {
-        type: 'boolean',
-      }
-    });
+// Declare
+const bfCheckNamespace = ( name ) => {
+  const namespace = [ { name: 'core/' }, { name: 'beflex/' } ];
+  for ( let i = 0; namespace.length > i; i++) {
+    if ( name.startsWith( namespace[i].name ) ) {
+      return true;
+    }
   }
-  return settings;
+  return false;
 }
+
+const bfHideOnMobileAttributes = ( settings, name ) => {
+  if ( ! bfCheckNamespace(name) ) {
+    return settings;
+  }
+  return Object.assign( {}, settings, {
+    attributes: Object.assign( {}, settings.attributes, {
+      hideOnMobile: { type: 'boolean' }
+    } ),
+  } );
+};
 
 wp.hooks.addFilter(
   'blocks.registerBlockType',
-  'beflex/hide-on-mobile',
-  hideOnMobileAttribute
-)
+  'beflex/hide-on-mobile-attributes',
+  bfHideOnMobileAttributes
+);
 
-const hideOnMobileControls = wp.compose.createHigherOrderComponent((BlockEdit) => {
-  return (props) => {
-    const { Fragment } = wp.element;
-    const { ToggleControl } = wp.components;
+
+const { createHigherOrderComponent } = wp.compose;
+
+const bfHideOnMobileControls = createHigherOrderComponent( ( BlockEdit ) => {
+  return ( props ) => {
+    const { Fragment, useState } = wp.element;
+    const { ToggleControl, RadioControl, TextControl } = wp.components;
     const { InspectorAdvancedControls } = wp.blockEditor;
     const { attributes, setAttributes, isSelected } = props;
+    const { hideOnMobile } = attributes;
+
+    if ( ! bfCheckNamespace(props.name) ) {
+      return (
+        <BlockEdit { ...props } />
+      );
+    }
+
     return (
       <Fragment>
         <BlockEdit {...props} />
         {isSelected &&
-        <InspectorAdvancedControls>
-          <ToggleControl
-            label={wp.i18n.__('Hide on mobile', 'beflex')}
-            checked={!!attributes.hideOnMobile}
-            onChange={(newval) => setAttributes({ hideOnMobile: !attributes.hideOnMobile })}
-          />
-        </InspectorAdvancedControls>
+          <InspectorAdvancedControls>
+            <ToggleControl
+              label={wp.i18n.__('Hide on mobile', 'beflex')}
+              checked={!!hideOnMobile}
+              onChange={ () => setAttributes({ hideOnMobile: !hideOnMobile }) }
+            />
+          </InspectorAdvancedControls>
         }
       </Fragment>
     );
   };
-}, 'hideOnMobileControls');
+}, 'bfHideOnMobileControls');
 
 wp.hooks.addFilter(
   'editor.BlockEdit',
-  'beflex/hide-on-mobile-control',
-  hideOnMobileControls
+  'beflex/hide-on-mobile-controls',
+  bfHideOnMobileControls
+);
+
+const bfHideOnMobileProp = createHigherOrderComponent( ( BlockListBlock ) => {
+  return ( props ) => {
+    if ( ! bfCheckNamespace(props.name) ) {
+      return (
+        <BlockListBlock { ...props } />
+      );
+    }
+
+    return (
+      <BlockListBlock
+        { ...props }
+        className={ 'hide-on-mobile' }
+      />
+    );
+  };
+}, 'bfHideOnMobileProp' );
+
+wp.hooks.addFilter(
+  'editor.BlockListBlock',
+  'beflex/hide-on-mobile-prop',
+  bfHideOnMobileProp
 );
 
 
-function hideOnMobileClass(extraProps, blockType, attributes) {
+import classnames from 'classnames';
+const bfHideOnMobileDisplay = ( extraProps, blockType, attributes ) => {
   const { hideOnMobile } = attributes;
-
-  if (typeof hideOnMobile !== 'undefined' && hideOnMobile) {
-    extraProps.className = extraProps.className + ' hide-on-mobile';
+  if ( hideOnMobile ) {
+    extraProps.className = classnames( extraProps.className, 'hide-on-mobile'  );
   }
+
   return extraProps;
-}
+};
 
 wp.hooks.addFilter(
   'blocks.getSaveContent.extraProps',
-  'beflex/hide-on-mobile-class',
-  hideOnMobileClass
+  'beflex/hide-on-mobile-display',
+  bfHideOnMobileDisplay
 );
